188c9c15769ec3f37d649882c22217cf2c591d3d
kapilkumar88 <kapil.saini6239@gmail.com>
2024-08-02 16:53:37 +0530

    done with the social login

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,24 @@
-import NextAuth from "next-auth"
+import NextAuth, { type DefaultSession } from "next-auth"
 import authConfig from "./auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
+
+
+declare module "next-auth" {
+    /**
+     * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    interface Session {
+        user: {
+            role: "ADMIN" | "USER";
+            name: string;
+            email: string;
+            image: string;
+        } & DefaultSession["user"]
+    }
+}
+
 
 export const {
     auth,
@@ -9,6 +26,38 @@ export const {
     signIn,
     signOut
 } = NextAuth({
+    callbacks: {
+        // async signIn({ user }) {
+        //     console.log(user);
+        //     const existingUser = await getUserById(user.id);
+
+        //     if (!existingUser || !existingUser.emailVerified) {
+        //         return false;
+        //     }
+
+        //     return true;
+        // },
+        async session({ token, session }) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+            }
+            if (token.role && session.user) {
+                session.user.role = token.role as "ADMIN" | "USER";
+            }
+            return session;
+        },
+        async jwt({ token }) {
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+
+            if (!existingUser) return token;
+
+            token.role = existingUser.role;
+
+            return token;
+        }
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: 'jwt' },
     ...authConfig
